66d4cd0e3fd2137856f277803df0178d8ceec935
Cyril Grima <cgrima@freeze.ig.utexas.edu>
2019-07-16 10:09:34 -0500

    init compliance with python3

--- __init__.py
+++ __init__.py
@@ -4,12 +4,6 @@ Author: C. Grima (cyril.grima@gmail.com)
 
 __author__ = 'Cyril Grima'
 
-#__all__ = ['do', 'get', 'raw', 'read', 'utils']
-
 __all__ = ['do', 'get', 'read']
 
-import do
-import get
-#import raw
-import read
-#import utils
+from . import do, get, read
